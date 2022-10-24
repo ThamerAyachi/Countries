ad80a12c8548d19841ca1301304255cfed1a593c
Thamer Ayachi <thamerjak3@gmail.com>
2022-10-24 16:52:03 +0100

    add back button

--- src/views/SingleView.vue
+++ src/views/SingleView.vue
@@ -1,5 +1,30 @@
 <template>
-	<div>{{ countryName }}</div>
+	<div class="xl:mx-52 mx-5 my-10">
+		<!-- button to go back -->
+		<div class="flex">
+			<router-link
+				to="/"
+				class="flex items-center space-x-3 shadow-md rounded bg-white py-2 px-7"
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-5 h-5"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+					/>
+				</svg>
+
+				<span>Back</span></router-link
+			>
+		</div>
+	</div>
 </template>
 
 <script>
