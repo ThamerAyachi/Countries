dd6d8caf8618d2f5ff3e9696ab55022ccba6d2ee
Thamer Ayachi <thamerjak3@gmail.com>
2022-10-24 16:35:07 +0100

    create new page for single country

---
+++ src/views/SingleView.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>{{ countryName }}</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			countryName: this.$route.params.name.replaceAll("_", " "),
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
